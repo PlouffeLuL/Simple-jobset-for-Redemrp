79a760cffc2cceec3dcbdd63bef1a67b20e5b4c3
PlouffeLuL <56097826+PlouffeLuL@users.noreply.github.com>
2020-01-03 14:17:57 -0500

    Delete Society.sql

--- Society.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS `society_money` (
-	`society` varchar(22) NOT NULL,
-	`name` varchar(22) NOT NULL,
-	`money` MEDIUMINT(3) NOT NULL DEFAULT '0',
-
-	PRIMARY KEY (`name`)
-);
